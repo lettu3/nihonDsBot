279d38710d7c1854b64215e03c4fd8e8c5457881
Ignacio <ignas.gomezb12@gmail.com>
2024-12-09 20:43:45 -0300

    static tasks, y config ahora contempla intents

--- nihonBot/NihonBot.cs
+++ nihonBot/NihonBot.cs
@@ -12,17 +12,38 @@ namespace NihonBot
 
         public static async Task Main ()
         {
-            _client = new DiscordSocketClient();
-            
-            //config token
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.DirectMessages
+            };
+            _client = new DiscordSocketClient(config);
+
+            _client.Log += Log;
+            _client.MessageReceived += HandleMessageAsync;
+            // Config token
             var reader = new JSONReader();
             await reader.ReadJSON();
-
 
             await _client.LoginAsync(TokenType.Bot, reader.Token);
             await _client.StartAsync();
             await Task.Delay(-1);
         }
         
+        private static Task Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+
+        private static async Task HandleMessageAsync(SocketMessage message)
+        {
+            if (message.Author.IsBot) return;
+
+            if (message.Content.ToLower() == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
     }
 }
