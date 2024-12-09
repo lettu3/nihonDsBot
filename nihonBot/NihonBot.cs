dbe356d144558835d3d6a8154c79b511cf3784c7
Ignacio <ignas.gomezb12@gmail.com>
2024-12-09 20:14:14 -0300

    IT'S ALIVE

---
+++ nihonBot/NihonBot.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using NihonBot.config;
+
+namespace NihonBot
+{
+    public class Program
+    {
+        private static DiscordSocketClient _client;
+
+        public static async Task Main ()
+        {
+            _client = new DiscordSocketClient();
+            
+            //config token
+            var reader = new JSONReader();
+            await reader.ReadJSON();
+
+
+            await _client.LoginAsync(TokenType.Bot, reader.Token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+        
+    }
+}
---
+++ nihonBot/config/JSONReader.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using System.IO; 
+using System.Threading.Tasks; 
+
+
+namespace NihonBot.config
+{
+    internal class JSONReader
+    {
+        public string Token {get; private set;}
+
+        public async Task ReadJSON(){
+            try
+            {
+                string filePath = "config/config.json";
+
+                string jsonContent = await File.ReadAllTextAsync(filePath);
+
+                var jsonDocument = JsonDocument.Parse(jsonContent);
+
+                if (jsonDocument.RootElement.TryGetProperty("token", out var tokenElement)){
+                    Token = tokenElement.GetString();
+                }
+                else
+                {
+                    throw new KeyNotFoundException("El campo 'token' no existe en el JSON.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al leer el JSON: {ex.Message}");
+                throw;
+            }
+        }
+    }
+
+}
